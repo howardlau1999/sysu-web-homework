afb84367219d49a0266cf53fa8bb459f4cc3f5d7
Howard Lau <howardlau1999@hotmail.com>
2018-10-20 10:05:23 +0800

    more elegant

--- 07-fifteen-puzzle/scripts/puzzle.js
+++ 07-fifteen-puzzle/scripts/puzzle.js
@@ -1,10 +1,3 @@
-var PuzzleGame = {
-    img_url: "./images/mario.jpg",
-    level: 4,
-    width: 640,
-    height: 640,
-
-};
 const Direction = {
     UP: 0,
     DOWN: 1,
@@ -19,25 +12,26 @@ const Move = {
 }
 
 function move(dir) {
+    var puzzle = this.puzzle;
+    var PuzzleGame = puzzle.PuzzleGame;
     this.row += Move.dr[dir];
     this.col += Move.dc[dir];
     this.style.marginTop = this.row * PuzzleGame.block_height + "px";
     this.style.marginLeft = this.col * PuzzleGame.block_width + "px";
 }
 
-function move_to_empty() {
-    var puzzle = document.getElementById("puzzle");
+function move_to_empty(dir) {
+    var puzzle = this.puzzle;
+    var PuzzleGame = puzzle.PuzzleGame;
     for (const key in Direction) {
         if (Direction.hasOwnProperty(key)) {
             const dir = Direction[key];
             var nr = this.row + Move.dr[dir];
             var nc = this.col + Move.dc[dir];
             if (nr < 0 || nr >= PuzzleGame.level || nc < 0 || nc >= PuzzleGame.level) continue;
-            if (puzzle.blocks[nr][nc] == -1) {
-                puzzle.blocks[nr][nc] = this.row * PuzzleGame.level + this.col;
-                puzzle.blocks[this.row][this.col] = -1;
-                puzzle.block_items[nr][nc] = this;
-                puzzle.block_items[this.row][this.col] = null;
+            if (!puzzle.blocks[nr][nc]) {
+                puzzle.blocks[nr][nc] = this;
+                puzzle.blocks[this.row][this.col] = null;
                 puzzle.empty_row = this.row;
                 puzzle.empty_col = this.col;
                 this.move(dir);
@@ -47,30 +41,57 @@ function move_to_empty() {
     }
 }
 
-function init() {
+function shuffle() {
+    var puzzle = this;
+    var PuzzleGame = this.PuzzleGame;
+    var shuffle_times = Math.floor(Math.random() * 100) * PuzzleGame.level + 200 * PuzzleGame.level;
+    for (var i = 0; i <= shuffle_times; ++i) {
+        var nr, nc;
+        do {
+            var dir = Math.floor(Math.random() * 4);
+            nr = puzzle.empty_row + Move.dr[dir];
+            nc = puzzle.empty_col + Move.dc[dir];
+        } while (nr < 0 || nr >= PuzzleGame.level || nc < 0 || nc >= PuzzleGame.level);
+        puzzle.blocks[nr][nc].move_to_empty();
+    }
+}
+
+function check_finished() {
+    var puzzle = this;
+    var PuzzleGame = this.PuzzleGame;
+    if (puzzle.blocks[PuzzleGame.level - 1][PuzzleGame.level - 1]) return false;
+    for (var row = 0; row < PuzzleGame.level; ++row) {
+        for (var col = 0; col < PuzzleGame.level; ++col) {
+            if (row == PuzzleGame.level - 1 && col == PuzzleGame.level - 1) break;
+            if (puzzle.blocks[row][col].block_id != row * PuzzleGame.level + col) return false;
+        }
+    }
+    return true;
+}
+
+function init(PuzzleGame) {
     var puzzle = document.getElementById("puzzle");
-    (function() {
+
+    (function () {
         var last;
         while (last = puzzle.lastChild) puzzle.removeChild(last);
-    })()
+    })();
+
     puzzle.style.width = PuzzleGame.width + "px";
     puzzle.style.height = PuzzleGame.height + "px";
     PuzzleGame.block_width = PuzzleGame.width / PuzzleGame.level;
     PuzzleGame.block_height = PuzzleGame.height / PuzzleGame.level;
     puzzle.blocks = [];
-    puzzle.block_items = [];
     puzzle.empty_row = PuzzleGame.level - 1;
     puzzle.empty_col = PuzzleGame.level - 1;
     for (var row = 0; row < PuzzleGame.level; ++row) {
-        var row_data = [];
         var row_items = [];
         for (var col = 0; col < PuzzleGame.level; ++col) {
             if (row == PuzzleGame.level - 1 && col == PuzzleGame.level - 1) {
-                row_data.push(-1);
                 row_items.push(null);
                 break;
             }
-            
+
             var block = document.createElement("div");
             block.className = "block";
             block.style.width = PuzzleGame.block_width + "px";
@@ -81,6 +102,7 @@ function init() {
             block.style.backgroundPositionY = -PuzzleGame.block_height * row + "px";
             block.row = row;
             block.col = col;
+            block.block_id = row * PuzzleGame.level + col;
             block.style.marginTop = block.row * PuzzleGame.block_height + "px";
             block.style.marginLeft = block.col * PuzzleGame.block_width + "px";
 
@@ -88,30 +110,34 @@ function init() {
             block.move_to_empty = move_to_empty;
             block.onclick = (e) => {
                 e.target.move_to_empty();
+                if (puzzle.check_finished()) setTimeout(() => {
+                    alert("You Win!")
+                }, 200);
             }
 
-            row_data.push(col + row * PuzzleGame.level);
             row_items.push(block);
+            block.puzzle = puzzle;
             puzzle.appendChild(block);
         }
-        puzzle.block_items.push(row_items);
-        puzzle.blocks.push(row_data);
+        puzzle.blocks.push(row_items);
     }
-    shuffle();
+    puzzle.shuffle = shuffle;
+    puzzle.check_finished = check_finished;
+    puzzle.PuzzleGame = PuzzleGame;
+    puzzle.shuffle();
 }
 
-function shuffle() {
-    var puzzle = document.getElementById("puzzle");
-    var shuffle_times = Math.floor(Math.random() * 100) * PuzzleGame.level + 200 * PuzzleGame.level;
-    for (var i = 0; i <= shuffle_times; ++i) {
-        var nr, nc;
-        do {
-            var dir = Math.floor(Math.random() * 4);
-            nr = puzzle.empty_row + Move.dr[dir];
-            nc = puzzle.empty_col + Move.dc[dir];
-        } while (nr < 0 || nr >= PuzzleGame.level || nc < 0 || nc >= PuzzleGame.level);
-        puzzle.block_items[nr][nc].move_to_empty();
-    }
-}
 
-window.onload = init;
+
+window.onload = () => {
+    var default_init = () => {
+        init({
+            img_url: "./images/panda.jpg",
+            level: 4,
+            width: 352,
+            height: 352,
+        })
+    };
+    document.getElementById("restart").addEventListener("click", default_init);
+    default_init();
+};
